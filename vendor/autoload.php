fdda27b21fe0a2b6e17f6ca2eb3f8ffdb1b82cf9
ValentinBretel <bretel.val@gmail.com>
2019-03-06 14:12:53 +0100

    TU - maj

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,6 +4,14 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
+require_once __DIR__ .'src/Email.php';
+require_once __DIR__ .'tests/EmailTest.php';
 
+spl_autoload_register(function ($class_name) {
+    include $class_name . '.php';
+});
+
+$obj  = new Email('bart.simpson@outlook.com');
+$obj2 = new EmailTest(); 
 
 return ComposerAutoloaderInit7c2cd7b71af6aef9402abee1840b006f::getLoader();
